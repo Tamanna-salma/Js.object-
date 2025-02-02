c29ed0c24e93acdd586eca82764328d2439a1aa0
Tamanna-salma <tamannaummey26@gmail.com>
2025-02-02 10:49:29 +0600

    index.js file added

---
+++ index.js
@@ -0,0 +1,147 @@
+// Object***
+// property or key
+
+/*let myName = 'tamanna';
+let age =27;
+let address = 'kishoreganj';
+let mySubjects = ['accounting','managment','corporate','english'];
+let ispassed = true;
+
+let person = {
+
+    name        : "Mithe",
+    age         : 25,
+    profession  : "Student",
+    isReading   : false,
+    mySubjects  : ['accounting','managment','corporate','english']
+
+};
+// console.log(person);*/
+
+// ১. dot notation***
+ /*console.log(person.name);
+ console.log(person.isReading);*/
+
+// Bracket notation**
+
+/*console.log(person['name']);
+console.log(person['isReading']);
+
+let id = person.name;
+console.log(id);
+let id2 = person['age'];
+console.log(id2);*/
+
+// 3.নতুন প্রপার্টি যোগ করা:
+
+/*person.hobby ='dustami'
+console.log(person);
+delete person.age ;
+console.log(person);*/
+
+
+/*let information = {
+      Name    :  "salma",
+      age     :  27,
+      village : 'rashidabad',
+      dist    : "kishoreganj" ,
+      hight   :  "4'11",   
+      hobby   :  ['slepping'],
+      
+};
+ console.log(information);
+
+//  dot notation****
+
+console.log(information.hobby);
+console.log(information.village);
+
+// Bracket notation******
+console.log(information['hight']);
+console.log(information['Name'])
+console.log(information['village']);
+console.log(information['dist']);
+console.log(information['age']);
+
+
+// 3.নতুন প্রপার্টি যোগ করা:****
+
+information.Gmail = 'salama@gmail.com'
+information.professtion ='student'
+console.log(information.Gmail);
+console.log(information.professtion);
+
+// 4.প্রপার্টি আপডেট করা:*****
+
+information.age = 28;
+information.hobby = 'codeding'
+information.Name ='tamanna'
+console.log(information.age);
+console.log(information.hobby);
+
+
+// 5.প্রপার্টি মুছে ফেলা****
+
+delete information.village;
+console.log(information);*/
+// *****
+
+
+
+/*let person = {
+
+    name        : "Mithe",
+    age         : 25,
+    profession  : "Student",
+    isReading   : false,
+    mySubjects  : ['accounting','managment','corporate','english']
+
+};
+// Object.keys()****
+
+/*const sima = Object.keys(person);
+console.log(sima);*/
+
+// Object.values()*****
+/*const simavalue = Object.values(person);
+console.log(simavalue);*/
+ 
+// Object.entries()*****
+/*const fulldata = Object.entries(person);
+console.log(fulldata);*/  
+
+/*const person1 = {
+            name            : 'mithi',
+            age             : 26,
+            fathaersname    : "md Rashid",
+            occution        : 'student',
+            subject         :  ['accounting','managment','corporate','english'],
+            address          :{
+                            Road no   :163,
+                            House    : 'roshidabad',
+                            dist     : 'kishoreganj',
+                            status   : {
+                                ismarried :true,
+                                children   : ['jodu','modu','kodu'],
+                            }
+
+
+            }
+
+
+
+};
+
+ console.log(person1.address.House);
+console.log(person1.subject[3]);
+console.log(person1.address.status.children[2]);*/  
+
+
+/*let subject = ['accounting','managment','corporate','english'];
+for (let i=0; i<subject.length; i++){
+    console.log(i);
+}
+for (const sub of subject) {                                           
+    console.log(sub);
+}*/
+
